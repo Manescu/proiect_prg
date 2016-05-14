b44be69fd3a50f442d32bd3d9d0f8015396f453b
Manescu <manescu.alex0@gmail.com>
2016-05-14 17:40:47 +0300

    Add files via upload

---
+++ proiect V1/into_tija.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+// a=marimea tijei
+
+int intro_tija()
+{ float n, pret_total,pret_pe_metru ;
+pret_pe_metru=25;
+pret_total=pret_pe_metru*n;
+
+    printf("intoduceti dimensiunea tijei(in metri) : ");
+    scanf("%f",&n);
+
+    for (i=1;i<=n;i++)
+    {
+
+    }
+}
---
+++ proiect V1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "into_tija.h"
+int main()
+{
+    intro_tija();
+    return 0;
+}
